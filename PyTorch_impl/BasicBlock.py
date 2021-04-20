0e82e62f3b1ccf084a831dd6f8a0b8f287a6b0b2
yindaheng98 <yindaheng98@163.com>
2021-04-21 00:03:49 +0800

    做好了一个框架和几个算子，但这个框架有问题

--- PyTorch_impl/BasicBlock.py
+++ PyTorch_impl/BasicBlock.py
@@ -41,7 +41,7 @@ class BasicBlock(nn.Module):
 
         return out
 
-    def TRT_export(self, constructor: TRT_Constructor, x):
+    def TRT_export(self, constructor: TRT_Constructor, x, residual=None):
         if residual is None:
             residual = x
 
@@ -52,7 +52,7 @@ class BasicBlock(nn.Module):
         out = constructor.Conv2d(self.conv2, out)
         out = constructor.BatchNorm2d(self.bn2, out)
 
-        out += residual
+        out = constructor.Add(out, residual)
         out = constructor.ReLU(self.relu, out)
         return out
 
@@ -62,12 +62,60 @@ if __name__ == '__main__':
 
     input_channel = 2
     output_channel = 2
+    h = 5
+    w = 5
+    batch_size = 2
     m = BasicBlock(input_channel, output_channel) # Pytorch构建的模型
 
-    data        = np.arange(2*input_channel*3*3,dtype=np.float32).reshape(2,input_channel,3,3)
+    logger = trt.Logger(trt.Logger.INFO)
+    builder                     = trt.Builder(logger)
+    network                     = builder.create_network(1<<int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))  #
+    profile                     = builder.create_optimization_profile()         #
+    config                      = builder.create_builder_config()               #
+    config.max_workspace_size   = 1 << 30                                       #
+    config.flags                = 0                                             #
+
+    inputT0     = network.add_input('inputT0', trt.DataType.FLOAT, (-1, input_channel, -1, -1))
+    profile.set_shape(inputT0.name, (1,input_channel,1,1),(10,input_channel,10,10),(100,input_channel,100,100))
+    config.add_optimization_profile(profile)
+    
+    constructor = TRT_Constructor(network)
+    output = m.TRT_export(constructor, inputT0)
+
+    network.mark_output(output)
+    engine = builder.build_engine(network, config)                              #
+    if engine == None:
+        exit()
+    
+    context         = engine.create_execution_context()
+    context.set_binding_shape(0,(batch_size,input_channel,h,w))
+    print("Bind0->", engine.get_binding_shape(0), context.get_binding_shape(0)) # 这里是
+    print("Bind1->", engine.get_binding_shape(1), context.get_binding_shape(1))
+
+    stream          = cuda.Stream()
+
+    data        = np.arange(2*input_channel*h*w,dtype=np.float32).reshape(batch_size,input_channel,h,w)*10+10
+    # data        = np.random.randn(batch_size,input_channel,h,w)*10+40
     inputH0     = np.ascontiguousarray(data.reshape(-1))
+    inputD0     = cuda.mem_alloc(inputH0.nbytes)
+    outputH0    = np.empty(context.get_binding_shape(1),dtype = trt.nptype(engine.get_binding_dtype(1)))    #
+    outputD0    = cuda.mem_alloc(outputH0.nbytes)
+        
+    cuda.memcpy_htod_async(inputD0, inputH0, stream)
+    context.execute_async_v2([int(inputD0), int(outputD0)], stream.handle)          #
+    cuda.memcpy_dtoh_async(outputH0, outputD0, stream)
+    stream.synchronize()
+    
+    print("inputH0:", data.shape,engine.get_binding_dtype(0))
+    print(data)
+    print("outputH0:", outputH0.shape,engine.get_binding_dtype(1))
+    print(outputH0)
 
     outputH0_torch = m(torch.Tensor(data))
     print("outputH0 in Pytorch:", outputH0_torch.shape)
     print(outputH0_torch)
 
+    diff = outputH0_torch.detach().numpy()-outputH0
+    print("Average absolute difference between Pytorch and TRT:", np.mean(np.abs(diff)))
+    print("Average relative difference between Pytorch and TRT:", np.mean(np.abs(diff/outputH0)))
+    print(diff)
--- PyTorch_impl/TRT_Constructor.py
+++ PyTorch_impl/TRT_Constructor.py
@@ -7,7 +7,7 @@ class TRT_Constructor:
     def __init__(self, network: trt.tensorrt.INetworkDefinition):
         self.network = network
 
-    def MaxPool2d(self, pool: nn.MaxPool2d, x):
+    def MaxPool2d(self, pool: nn.MaxPool2d, x: trt.tensorrt.ITensor):
         stride = pool.stride
         y = self.network.add_pooling(
             input=x,
@@ -16,22 +16,22 @@ class TRT_Constructor:
         )
         y.stride = pool.stride
         y.padding = pool.padding
-        return y
+        return y.get_output(0)
 
-    def Conv2d(self, conv: nn.Conv2d, x):
+    def Conv2d(self, conv: nn.Conv2d, x: trt.tensorrt.ITensor):
         y = self.network.add_convolution(
             input=x,
             num_output_maps=conv.out_channels,
             kernel_shape=conv.kernel_size,
-            kernel=conv.weight,
-            bias=conv.bias
+            kernel=conv.weight.detach().numpy(),
+            bias=conv.bias.detach().numpy() if conv.bias is not None else None
         )
         y.stride = conv.stride
         y.padding = conv.padding
         y.dilation = conv.dilation
-        return y
+        return y.get_output(0)
 
-    def BatchNorm2d(self, bn: nn.BatchNorm2d, x):
+    def BatchNorm2d(self, bn: nn.BatchNorm2d, x: trt.tensorrt.ITensor):
         eps = bn.eps
         gamma = bn.weight
         beta = bn.bias
@@ -42,11 +42,18 @@ class TRT_Constructor:
         y = self.network.add_scale(
             input=x,
             mode=trt.ScaleMode.CHANNEL,
-            shift=shift,
-            scale=scale
+            shift=shift.detach().numpy(),
+            scale=scale.detach().numpy()
         )
-        return y
+        return y.get_output(0)
 
-    def ReLU(self, relu: nn.ReLU, x):
-        y = self.network.add_parametric_relu(x)
-        return y
+    def ReLU(self, relu: nn.ReLU, x: trt.tensorrt.ITensor):
+        y = self.network.add_activation(x, trt.ActivationType.RELU)
+        return y.get_output(0)
+
+    def Elementwise(self, a: trt.tensorrt.ITensor, b: trt.tensorrt.ITensor, op: trt.tensorrt.ElementWiseOperation):
+        y = self.network.add_elementwise(a, b, op)
+        return y.get_output(0)
+
+    def Add(self, a: trt.tensorrt.ITensor, b: trt.tensorrt.ITensor):
+        return self.Elementwise(a, b, trt.tensorrt.ElementWiseOperation.SUM)
